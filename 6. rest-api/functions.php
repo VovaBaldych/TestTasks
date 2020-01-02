4dae79ddbf175a51b7fd0cdc6e61367ecc0f3ae6
VovaBaldych <48417424+VovaBaldych@users.noreply.github.com>
2020-01-02 11:14:19 +0200

    v 1.1
    Розніс код по функціям та файлам

---
+++ 6. rest-api/functions.php
@@ -0,0 +1,22 @@
+<?php
+    include 'init_database.php';
+
+    function generateNumber()
+    {
+        $digit = random_int(0, 1000);
+        echo '<h1>Ваше число - '.$digit.'</h1>';
+        $query = $pdo->exec('INSERT INTO generate(result) VALUES ("'.$digit.'")');          // записуємо згернероване число в БД
+    }
+
+    function retrieveNumber()
+    {
+        $get_id=end(explode('/', $_SERVER['REQUEST_URI']));                                                           
+        if(is_numeric($get_id))                                                             // перевіряємо, чи є це слово числом
+        {
+            $get_id_more = $pdo->prepare("SELECT result FROM generate WHERE id='$get_id'"); // якщо так, то робимо запит до БД
+            $get_id_more->execute();
+            $final = $get_id_more->fetch();
+            printf($r = '<h1 data-test="%1$d" data-more="%2$d">%1$d</h1>', ($final ? $final : 'Invalid'), 3);                 // якщо в БД ID співпадає з числом користувача - виводимо згенероване раніше число
+        }
+    }
+?>
--- 6. rest-api/index.php
+++ 6. rest-api/index.php
@@ -1,24 +1,13 @@
 <?php
-    require 'init_database.php';
+    include 'init_database.php';
+    include 'functions.php';
 
-    $pieces = explode('/', $_SERVER['REQUEST_URI']);                                        // ділимо URL на слова
-
-    if(strpos($_SERVER['REQUEST_URI'], 'generate', 1) === 1)                       // якщо в URL є обов'язковий підрядок 'rest-api/generate'
+    if(strpos($_SERVER['REQUEST_URI'], 'rest-api/generate', 1) === 1)                       // якщо в URL є обов'язковий підрядок 'rest-api/generate'
     {
-        $digit = random_int(0, 1000);
-        echo '<h1>Ваше число - '.$digit.'</h1>';
-        $query = $pdo->exec('INSERT INTO generate(result) VALUES ("'.$digit.'")');          // записуємо згернероване число в БД
+        generateNumber();
     }
-    else if(strpos($_SERVER['REQUEST_URI'], 'get', 1) === 1)                       // якщо в URL є обов'язковий підрядок 'rest-api/get'
+    else if(strpos($_SERVER['REQUEST_URI'], 'rest-api/get', 1) === 1)                       // якщо в URL є обов'язковий підрядок 'rest-api/get'
     {
-        $get_id=end($pieces);                                                               // беремо останнє слово з рядка $pieces
-        if(is_numeric($get_id))                                                             // перевіряємо, чи є це слово числом
-        {
-            $get_id_more = $pdo->prepare("SELECT result FROM generate WHERE id='$get_id'"); // якщо так, то робимо запит до БД
-            $get_id_more->execute();
-            $final = $get_id_more->fetch();
-            printf($r = '<h1 data-test="%1$d" data-more="%2$d">%1$d</h1>', ($final ? $final : 'Invalid'), 3);                 // якщо в БД ID співпадає з числом користувача - виводимо згенероване раніше число
-        }
-    }
-    else {echo 'Sorry, but no!';}
+        retrieveNumber();
+    } else {echo 'Sorry, but no!';}
 ?>
